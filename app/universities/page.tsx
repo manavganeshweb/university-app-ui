98851811bced14caf42d264d45f4c6eb39b33187
manavganeshweb <manavganeshh@gmail.com>
2026-07-31 16:00:00 +0530

    page-edit

--- app/universities/page.tsx
+++ app/universities/page.tsx
@@ -1,19 +1,22 @@
 import {universities} from "../../lib/universities"
 import UniversitiesClient from "./UniversitiesClient"
-
+import { Header } from "../../components/Header"
+import Footer from "../../components/Footer"
 
 
 export default function UniversitiesPage(){
 
 
 return(
-
+<>
+<Header/>
 <UniversitiesClient
 
 universities={universities}
 
 />
-
+<Footer/>
+</>
 )
 
 }
